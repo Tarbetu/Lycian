3da82f89c354d62879a3e222b08079722def9b41
Emrecan Şuşter <emrecansuster@outlook.com>
2025-02-19 22:46:59 +0300

    Remove scope resolver, i will take the other way

--- analyzer/src/lib.rs
+++ analyzer/src/lib.rs
@@ -1,8 +1,5 @@
 mod resolution_error;
-mod scope_resolver;
 use parser::{Class, Literal, LiteralIndex, Name, NameIndex, Parser};
-
-use scope_resolver::{ScopeHierarchy, ScopeResolver};
 
 use ahash::AHashMap;
 use bimap::BiHashMap;
@@ -12,9 +9,9 @@ pub use resolution_error::ResolutionResult;
 /// The AnalysisPipeline struct contains all the analyzers and optimizers that are used to
 /// analyze the AST and produce a more optimized version of the AST.
 ///
-/// ScopeResolver: Revolves the scope hierarchy and names of a program.
+/// TypeChecker: A classic for AOT compiler, type checker checks and deduce the typing of the program.
 /// InheritanceResolver: Lycian staticly resolves inheritance and checks for cycles.
-/// TypeChecker: A classic for AOT compiler, type checker checks the typing of the program.
+/// ScopeResolver: Revolves the scope hierarchy and names of a program.
 /// MemoryAnalyzer: Lycian handles the memory statically instead of using a garbage collector.
 /// EffectAnalyzer: Lycian aims to be a pure language, but it allows effects for IO. Also we check the branches to determine if the code suits for GPU.
 /// Codegen and Optimizer: Lycian will have a backend, and we can use MLIR optimization passes.
@@ -31,9 +28,5 @@ impl AnalysisPipeline {
             literals: parser.literals,
             classes: parser.classes,
         }
-    }
-
-    pub fn resolve_scopes(&self) -> ResolutionResult<ScopeHierarchy> {
-        ScopeResolver::new(&self.names, &self.classes).resolve()
     }
 }
