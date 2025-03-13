0c6a3f64ed37e96276641c69cab5ed12adafd4e0
Emrecan Şuşter <emrecansuster@outlook.com>
2025-03-13 21:40:38 +0300

    Change the logic of type analyzer - adding typed ast v2

--- analyzer/src/type_analyzer/typed_ast/class.rs
+++ analyzer/src/type_analyzer/typed_ast/class.rs
@@ -1,10 +1,11 @@
-use super::super::r#type::ClassState;
 use super::TypedFunction;
+use crate::type_analyzer::{ClassState, Type};
 use ahash::HashMap;
 use parser::EntityIndex;
 
 pub struct TypedClass {
     pub class: parser::Class,
+    pub itself: Type,
     pub states: Vec<ClassState>,
     pub methods: HashMap<EntityIndex, Vec<TypedFunction>>,
 }
--- analyzer/src/type_analyzer/typed_ast/function.rs
+++ analyzer/src/type_analyzer/typed_ast/function.rs
@@ -6,6 +6,7 @@ use crate::type_analyzer::Type;
 
 pub struct TypedFunction {
     pub function: Function,
+    pub itself: Type,
     pub params: Vec<Pattern>,
     pub return_type: Option<Type>,
     pub body: TypedExpression,
