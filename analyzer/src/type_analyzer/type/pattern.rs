21c680c4705d924913c24d9b87491552dfa3eb1e
Emrecan Şuşter <emrecansuster@outlook.com>
2025-03-13 21:54:16 +0300

    Enables guards, adding documentation to patterns and adding PatternId

--- analyzer/src/type_analyzer/type/pattern.rs
+++ analyzer/src/type_analyzer/type/pattern.rs
@@ -2,6 +2,7 @@ use parser::{EntityIndex, LiteralIndex};
 
 use crate::{
     resolution_error::{TypeError, TypeErrorKind},
+    type_analyzer::typed_ast::TypedExpression,
     AnalysisPipeline,
 };
 
@@ -11,35 +12,42 @@ use super::PrimitiveType;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum PatternPrecedence {
     Literal = 0,
-    // Guard = 10,
+    Guard = 10,
     Function = 20,
     NoConstrain = 255,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum PatternId {
+    Entity(EntityIndex),
+    ClassSelf,
+    NoName,
 }
 
 pub enum Pattern {
     /// A pattern that is only constrained by the function of the entity
     /// Every type is a function in Lycian
-    /// Example: `x: MyFunc`
+    /// Example: `x: MyFunc` (Named), `self: MyFunc` (Points to the 'self' state) or `MyFunc` (No name)
     FunctionConstrained {
-        entity: EntityIndex,
+        id: PatternId,
         function_constraint: EntityIndex,
     },
     /// A pattern that is constrained by a literal value
-    /// Example: `x: 5`
+    /// Example: `x: 5` (Named), `self: 5` (Points the to the 'self' state) or `5` (No name)
     LiteralConstrained {
-        entity: EntityIndex,
+        id: PatternId,
         base_type: PrimitiveType,
         literal_value: LiteralIndex,
     },
-    // A pattern that is constrained by a condition
-    // A function constraint is also present
-    // Example: `x: Integer when x > 5`
-    // To be implemented
-    // GuardConstrained {
-    //     entity: EntityIndex,
-    //     base_class: EntityIndex,
-    //     guard: parser::Expression,
-    // },
+    /// A pattern that is constrained by a condition
+    /// A function constraint is also present
+    /// The guard is a TypedExpression that is a boolean
+    /// Example: `x: Integer when x > 5` (Named), `self: Integer when self > 5` (Points the to the 'self' state) or `Integer when self > 5` (No name)
+    GuardConstrained {
+        id: PatternId,
+        base_class: EntityIndex,
+        guard: TypedExpression,
+    },
     /// A pattern that is not constrained in any way
     /// Nothing is present, so the entity can be any value
     /// The type is deduced in the context of the Match expr
@@ -84,7 +92,7 @@ impl From<&Pattern> for PatternPrecedence {
     fn from(value: &Pattern) -> Self {
         match value {
             Pattern::LiteralConstrained { .. } => PatternPrecedence::Literal,
-            // Pattern::GuardConstrained { .. } => PatternPrecedence::Guard,
+            Pattern::GuardConstrained { .. } => PatternPrecedence::Guard,
             Pattern::FunctionConstrained { .. } => PatternPrecedence::Function,
             Pattern::NoConstrain { .. } => PatternPrecedence::NoConstrain,
         }
