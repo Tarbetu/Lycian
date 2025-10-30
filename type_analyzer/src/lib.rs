aa1303a98d6893e53fcca2941a0711060c492f50
Emrecan Suster <emrecan.suster@tuta.io>
2025-10-30 18:51:27 +0300

    fix(type_analyzer): Fixing unneeeded type error

--- type_analyzer/src/lib.rs
+++ type_analyzer/src/lib.rs
@@ -11,5 +11,5 @@ use hierarchy::Hierarchy;
 pub fn build_type_hierarchy<'a>(
     scope_hierarchy: scope::Hierarchy<'a>,
 ) -> TypeResult<Hierarchy<'a>> {
-    Ok(Hierarchy::new(scope_hierarchy))
+    Hierarchy::new(scope_hierarchy)
 }
